dc2309c7e642e597c971dc107cc0bc52878e0949
nelsonquillay <nmq_26@hotmail.com>
2025-11-27 03:55:51 -0300

    clases

---
+++ 06_request/02_requests.py
@@ -0,0 +1,145 @@
+# Cómo hacer peticiones a APIs con Python
+# con y sin dependencias
+
+# 1. Sin dependencias (díficil y sin dependencias)
+import urllib.request
+import json
+
+api_posts = "https://jsonplaceholder.typicode.com/posts/"
+
+try:
+    response = urllib.request.urlopen(api_posts)
+    data = response.read()
+    json_data = json.loads(data.decode("utf-8"))
+    print(json_data)
+    response.close()
+except urllib.error.URLError as e:
+    print(f"Error al hacer la petición: {e}")   
+
+# 2. Con dependencia (requests)
+
+import requests
+
+print("\nGET:") # Haciendo una petición GET
+api_posts = "https://jsonplaceholder.typicode.com/posts/" # URL de la API
+response = requests.get(api_posts) # Haciendo la petición GET
+#print(response.json()) # Imprime la respuesta en formato JSON
+json = response.json() # Guarda la respuesta en una variable
+print(json[0]) # Imprime el primer elemento de la respuesta
+
+
+# 3 un POST con requests
+print("\nPOST:") # Haciendo una petición POST
+
+'''api_posts = "https://jsonplaceholder.typicode.com/posts/" # URL de la API
+
+input = {
+    "title": "Nelson",
+    "body": "Quillay",
+    "userId": 26
+} # Datos a enviar en la petición POST
+
+response = requests.post(api_posts, json = input) # Haciendo la petición POST'''
+
+try:
+    response = requests.post(
+        "https://jsonplaceholder.typicode.com/posts/",
+        json = {
+            "title": "Nelson",
+            "body": "Quillay",
+            "userId": 26
+        }
+    ) 
+
+    print(response.json()) #
+    print(response.status_code) # Imprime el código de estado de la respuesta
+
+except requests.exceptions.RequestException as e:
+    print(f"Error al hacer la petición: {e}")   
+
+# 4. Un PUT
+print("\nPUT:") # Haciendo una petición PUT
+
+try:
+    response = requests.put(
+        "https://jsonplaceholder.typicode.com/posts/1",#/posts/1 para actualizar el post con id 1
+        json = {
+            "title": "fooball",
+            "body": "bar",
+            "userId": 1,
+        }
+    )
+    print(response.json()) #
+    print(response.status_code) # Imprime el código de estado de la respuesta
+
+except requests.exceptions.RequestException as e:
+    print(f"Error al hacer la petición: {e}")
+
+# un PATCH solo para actualizar un campo del objeto
+print("\nPATCH:") # Haciendo una petición PATCH
+try:
+    response = requests.patch(
+        "https://jsonplaceholder.typicode.com/posts/1",#/posts/1 para actualizar el post con id 1
+        json = {
+            "title": "Hectorrrrrr",
+        }
+    )
+    print(response.json()) #
+    print(response.status_code) # Imprime el código de estado de la respuesta
+except requests.exceptions.RequestException as e:
+    print(f"Error al hacer la petición: {e}")   
+
+# Usar la API de GPT-4o de OpenAI
+# Ref: https://platform.openai.com/docs/api-reference/making-requests
+
+import json
+import requests
+
+OPENAI_KEY = "REDACTED_OPENAI_KEY"
+
+def call_openai_gpt(api_key, prompt):
+  url = "https://api.openai.com/v1/chat/completions"
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+  data = {
+    "model": "gpt-4o-mini",
+    "messages": [{"role": "user", "content": prompt}]
+  }
+
+  response = requests.post(url, json=data, headers=headers)
+  return response.json()
+
+api_response = call_openai_gpt(OPENAI_KEY, "Escribe un breve poema sobre la programación")
+
+# print(json.dumps(api_response, indent=2))
+
+print(api_response["choices"][0]["message"]["content"])
+
+# Llamar a la API de DEEPSEEK
+
+import json
+
+DEEPSEEK_API_KEY = "REDACTED_DEEPSEEK_KEY"
+
+def call_deepseek(api_key, prompt):
+  url = "https://api.deepseek.com/chat/completions"
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+  data = {
+    "model": "deepseek-chat",
+    "messages": [{"role": "user", "content": prompt}]
+  }
+
+  response = requests.post(url, json=data, headers=headers)
+  print(response.json())
+  return response.json()
+
+api_response = call_deepseek(DEEPSEEK_API_KEY, "Escribe un breve poema sobre la programación")
+
+# print(json.dumps(api_response, indent=2))
+
+print(api_response["choices"][0]["message"]["content"])
---
+++ 06_request/03_clases.py
@@ -0,0 +1,8 @@
+# 1. Introducción a las Clases en Python
+# Las clases son plantillas para crear objetos. Un objeto es una instancia de una clase.
+# Nos permite agrupar datos (atributos o propiedades) y funciones (métodos) en un solo lugar.
+
+# Ejemplo básico de una clase
+class Coche:
+    # atributo de clase (comparte todas las instancias)
+    tipo = "vehículo de cuatro ruedas" 
